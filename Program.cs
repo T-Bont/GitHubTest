f621a4cd4038d0c948f0f14bfe0b75c5f9bfe70f
tjbbo <tjbbo@LAPTOP-NF2FACNC>
2024-10-14 10:19:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GitHubTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Test");
+        }
+    }
+}
